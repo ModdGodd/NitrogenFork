93bd6873a7879b2f2ca5ee39cb5f722509f8e343
ModdGodd <98932563+ModdGodd@users.noreply.github.com>
2025-05-06 17:55:11 -0700

    added the rest of functions too

--- preload.js
+++ preload.js
@@ -1,10 +1,18 @@
-// preload.js
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-    executeScript: (code) => ipcRenderer.invoke('execute-script', code),
-    getLocalScripts: () => ipcRenderer.invoke('get-local-scripts'),
-    toggleAutoExec: (name, enabled) => ipcRenderer.invoke('toggle-autoexec', { name, enabled }),
-    getScripts: (term) => ipcRenderer.invoke('get-scripts', term),
-    startLogMonitoring: () => ipcRenderer.send('start-log-monitoring')
+  executeScript: script => ipcRenderer.invoke('execute-script', script),
+  getScripts: q      => ipcRenderer.invoke('get-scripts', q),
+  openRoblox: ()     => ipcRenderer.invoke('open-roblox'),
+  saveScript: (n,c,a)=> ipcRenderer.invoke('save-script', n, c, a),
+  toggleAutoExec: (n,e) => ipcRenderer.invoke('toggle-autoexec', n, e),
+  getLocalScripts: () => ipcRenderer.invoke('get-local-scripts'),
+  deleteScript: n => ipcRenderer.invoke('delete-script', n),
+  renameScript: (o,n) => ipcRenderer.invoke('rename-script', o, n),
+  startLogMonitoring: ()  => ipcRenderer.send('start-log-monitoring'),
+  stopLogMonitoring: ()   => ipcRenderer.send('stop-log-monitoring'),
+  setLogRefreshRate: r    => ipcRenderer.send('set-log-refresh-rate', r),
+  onLogUpdate: cb         => ipcRenderer.on('log-update', (_,lines) => cb(lines)),
+  quitApp: ()             => ipcRenderer.invoke('quit-app'),
+  minimizeApp: ()         => ipcRenderer.invoke('minimize-app'),
 });
